e2a2812eddaf6fa68c2d523164cddccbfe0f2987
Abhishek Verma <abhishek.verma@brightscout.com>
2023-01-31 11:12:31 +0530

    [MI-2669]: Added block lines

--- webapp/src/utils/errorHandling.ts
+++ webapp/src/utils/errorHandling.ts
@@ -4,9 +4,11 @@ const getSubscribeModalCustomError = (errorState?: ApiErrorResponse) => {
     if (errorState?.status === 400 && errorState?.data.Error === pluginConstants.messages.error.subscriptionAlreadyExists) {
         return pluginConstants.messages.error.subscriptionAlreadyExists;
     }
+
     if (errorState?.status === 403 && errorState?.data.Error.includes(pluginConstants.messages.error.accessDenied)) {
         return pluginConstants.messages.error.adminAccessError;
     }
+
     if (errorState?.status === 500 && errorState?.data.Error.includes(pluginConstants.messages.error.failedToGetSubscriptions)) {
         return pluginConstants.messages.error.failedToGetSubscriptions;
     }
@@ -18,9 +20,11 @@ const getLinkProjectModalCustomError = (errorState?: ApiErrorResponse) => {
     if (errorState?.status === 404 || errorState?.status === 401) {
         return pluginConstants.messages.error.notAccessibleError;
     }
+
     if (errorState?.status === 500 && errorState?.data.Error.includes(pluginConstants.messages.error.errorExpectedForOAuthNotEnabled)) {
         return pluginConstants.messages.error.errorMessageOAuthNotEnabled;
     }
+
     return null;
 };
 
@@ -28,9 +32,11 @@ const confirmationModalCustomError = (errorState?: ApiErrorResponse) => {
     if (errorState?.status === 403 && errorState?.data.Error.includes(pluginConstants.messages.error.accessDenied)) {
         return pluginConstants.messages.error.adminAccessError;
     }
+
     if (errorState?.status === 404 && errorState?.data.Error.includes(pluginConstants.messages.error.subscriptionNotFound)) {
         return pluginConstants.messages.error.subscriptionNotFound;
     }
+
     return null;
 };
 
